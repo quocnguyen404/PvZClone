82368d2add7209666b6c16ddb5e40bce097309eb
quocnguyen404 <125229853+quocnguyen404@users.noreply.github.com>
2023-12-25 23:32:58 +0700

    panel ui handler

---
+++ Assets/_Scripts/UI/LoseUIHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseUIHandler : UIHandler
+{
+    [SerializeField] UIHandler panel = null;
+    [SerializeField] Button playAgainBtn = null;
+    [SerializeField] Button menuBtn = null;
+
+
+}
--- Assets/_Scripts/UI/UIHandler.cs
+++ Assets/_Scripts/UI/UIHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class UIHandler : MonoBehaviour
 {
     protected CanvasGroup cv = null;
